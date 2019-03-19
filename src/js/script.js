f404cff3c8dbdb7c6a34d0b867ce590bc2d465c8
Ganja Vasiliy <vasllly219@gmail.com>
2019-03-19 13:55:17 +0300

    FIX script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,7 @@
 export default function findBy(key, prop) {
   return function closures(obj) {
-    if (key === 'name') {
-      if (obj.name === prop) {
-        return true;
-      }
-    }
-    if (key === 'type') {
-      if (obj.type === prop) {
-        return true;
-      }
-    }
-    if (key === 'description') {
-      if (obj.description === prop) {
-        return true;
-      }
+    if (obj[key] === prop) {
+      return true;
     }
     return false;
   };
